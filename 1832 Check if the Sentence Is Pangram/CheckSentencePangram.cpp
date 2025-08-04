5b976e2b267c665d7b28e23a7197ddead5fd17b5
Theparamvrsingh <134386559+Theparamvrsingh@users.noreply.github.com>
2025-08-05 02:33:01 +0530

    Create CheckSentencePangram.cpp

---
+++ 1832 Check if the Sentence Is Pangram/CheckSentencePangram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int> alphabets(26, 0);
+        for(char c : sentence) alphabets[c-'a']++;
+        for(int i=0;i<26;i++){
+            if(alphabets[i] == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
